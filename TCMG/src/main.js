69dc9afc4b44219563dd9257e30dff54e684c5ef
Moshir <alexandermoshir@gmail.com>
2024-09-28 12:53:03 -1000

    removed files

--- TCMG/src/main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@assets/styles/global.css'; // Import global styles
-ReactDOM.render(React.createElement(React.StrictMode, null,
-    React.createElement(App, null)), document.getElementById('root'));
