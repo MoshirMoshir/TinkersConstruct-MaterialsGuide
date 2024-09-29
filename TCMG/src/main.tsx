00b3043d953adafe67ac57cd4408c2089926195b
Moshir <alexandermoshir@gmail.com>
2024-09-29 10:27:05 -1000

    fixed react-dom version

--- TCMG/src/main.tsx
+++ TCMG/src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '@assets/styles/global.css'; // Import global styles
+import '@assets/styles/global.css';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement!);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
