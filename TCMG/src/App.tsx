e752a787157edeb69443a6963ec49184629108e9
Moshir <alexandermoshir@yahoo.com>
2024-09-29 18:09:21 -1000

    Converted Browser Routing to Hash Routing
    fixes 404 refresh bug

--- TCMG/src/App.tsx
+++ TCMG/src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from '@pages/home/Home';
 import Version1_12_2 from '@pages/versions/1.12.2';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/versions/1.12.2" element={<Version1_12_2 />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/versions/1.12.2" element={<Version1_12_2 />} />
+    </Routes>
   );
 };
 
--- TCMG/src/main.tsx
+++ TCMG/src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { HashRouter as Router} from 'react-router-dom';  // Import HashRouter
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@assets/styles/global.css';
 
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement!);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+)
