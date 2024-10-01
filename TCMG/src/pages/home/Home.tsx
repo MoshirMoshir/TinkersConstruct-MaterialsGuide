b7417b34c5901714b276578b05dd35d2f4a3fa1e
Moshir <alexandermoshir@gmail.com>
2024-09-30 22:17:07 -1000

    Edited Home content

--- TCMG/src/pages/home/Home.tsx
+++ TCMG/src/pages/home/Home.tsx
@@ -9,12 +9,16 @@ const Home: React.FC = () => {
     <div className="home">
       <Navbar />
       <div className="home-content">
-        <h1>Welcome to Tinkers Tool Builder</h1>
-        <p>This web app allows you to build tools using different materials from Tinkers Construct. Select a version to get started!</p>
+        <h1>Welcome to the <br/>Tinker's Construct Material Guide!</h1>
+        <p>I created this webapp to help you navigate the hundreds of possible materials this wondrous mod contains!</p>
+        <p>
+        You can also engineer the tools of your dreams with ease! Whether it's optimizing for the best DPS or combining crazy modifiers for synergies, you'll be able to do it all right here in one place!
+        </p>
+        <p>Select a version to get started:</p>
         <div className="version-list">
           <Link to="/versions/1.12.2" className="version-card">
             <h2>Version 1.12.2</h2>
-            <p>Build and compare tools with materials available in Tinkers Construct 1.12.2</p>
+            <p>Browse materials and create tools available in Tinker's Construct 1.12.2</p>
           </Link>
           {/* Add more versions here */}
         </div>
