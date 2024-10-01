2d6542ab6b4740c14397ce9caff6fc63d52ecb59
Moshir <alexandermoshir@gmail.com>
2024-10-01 11:06:02 -1000

    Update Navbar.tsx

--- TCMG/src/components/navbar/Navbar.tsx
+++ TCMG/src/components/navbar/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar: React.FC = () => {
   {/* Middle: Nav Links */}
   <ul className="navbar-links">
     <li><Link to="/">Home</Link></li>
-    <li><Link to="/versions/1.12.2">Version 1.12.2</Link></li>
+    <li><Link to="/versions/1.12.2">1.12.2</Link></li>
     <li><Link to="/donate">Donate</Link></li>
     {/* Add more links as needed */}
   </ul>
